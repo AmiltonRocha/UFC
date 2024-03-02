2d681e00729377cd24cf1ade7139513ca6d6c978
AmiltonRocha <Amiltonrholanda@gmail.com>
2024-03-02 13:08:39 -0300

    Exercicio UFC

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     Lutador lutador1 = new Lutador("Jose", "Brasileira", 25, 1.75, 110, 20, 0, 5);
+
+     lutador1.apresentarLutador();
+
+       
+       
+       
+    }
+}
