9a278c1b21d2719c151565761749216c78c7446a
AmiltonRocha <Amiltonrholanda@gmail.com>
2024-03-04 18:52:58 -0300

    EX UFC

--- src/App.java
+++ src/App.java
@@ -1,11 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-     Lutador lutador1 = new Lutador("Jose", "Brasileira", 25, 1.75, 110, 20, 0, 5);
+    Lutador L[]= new Lutador[2];
+    L[0]  = new Lutador("Jose", "Brasileira", 25, 1.75, 110, 20, 0, 5);
+    L[1] = new Lutador("Tiago", "Brasileira", 25, 1.75, 110, 20, 0, 5);
+         
+    L[1].apresentarLutador(); 
 
-     lutador1.apresentarLutador();
+    Luta Comecar = new Luta();
+    Comecar.marcaLuta(L[0],L[1]);
+
+    Comecar.Lutar();
 
        
        
        
     }
 }
+
--- src/Luta.java
+++ src/Luta.java
@@ -1,23 +1,65 @@
+import java.util.Random;
+
 public class Luta {
-    private String desafiado;
-    private String desafiante;
+    private Lutador desafiado;
+    private Lutador desafiante;
     private int rounds;
-    private String aprovado;
+    private Boolean aprovado;
+        
+ 
 
     
-    public Luta(String desafiado, String desafiante, int rounds, String aprovado) {
-        this.desafiado = desafiado;
-        this.desafiante = desafiante;
-        this.rounds = rounds;
-        this.aprovado = aprovado;
+    public void  marcaLuta(Lutador lute1,Lutador lute2){
+       if((lute1.getCategoria() == lute2.getCategoria()) && lute1 != lute2){
+        this.aprovado = true;
+        this.desafiante = lute2;
+        this.desafiado = lute1;
+          
 
+       }else{
+        this.aprovado=false;
+        this.desafiado = null;
+        this.desafiante = null;
 
+        
+        
+       }
+    
     }
-    public String marcaLuta(){
-      
-     
-      
+
+    public void Lutar(){
+     if(this.aprovado){
+        System.out.println("LUTADOR1 : ");
+        this.desafiante.apresentarLutador();
+        System.out.println("LUTADOR2: ");
+        this.desafiado.apresentarLutador();
+        Random Acontecerluta = new Random();
+        int FIGHT = Acontecerluta.nextInt(3);
+        switch (FIGHT) {
+            case 0: 
+            System.out.println("Empatou");
+            this.desafiado.empatarLuta();
+            this.desafiante.empatarLuta();
+            break;
+
+             case 1:
+             System.out.println("Ganhou: "+this.desafiado.getNome());
+             this.desafiado.ganharLuta();
+             this.desafiante.perdeLuta();
+            break;
+
+             case 2:
+             System.out.println("Ganhou:"+this.desafiante.getNome());
+             this.desafiante.ganharLuta();
+             this.desafiado.perdeLuta();
+            break;
+        }
+        
+     }else{
+        System.out.print("Não ocorreu luta");
+     }
     }
+    
     
 
 
@@ -32,22 +74,16 @@ public class Luta {
 
 
 
-
-
-
-
-
-
-    public String getDesafiado() {
+    public Lutador getDesafiado() {
         return desafiado;
     }
-    public void setDesafiado(String desafiado) {
+    public void setDesafiado(Lutador desafiado) {
         this.desafiado = desafiado;
     }
-    public String getDesafiante() {
+    public Lutador getDesafiante() {
         return desafiante;
     }
-    public void setDesafiante(String desafiante) {
+    public void setDesafiante(Lutador desafiante) {
         this.desafiante = desafiante;
     }
     public int getRounds() {
@@ -56,10 +92,10 @@ public class Luta {
     public void setRounds(int rounds) {
         this.rounds = rounds;
     }
-    public String getAprovado() {
+    public Boolean getAprovado() {
         return aprovado;
     }
-    public void setAprovado(String aprovado) {
+    public void setAprovado(Boolean aprovado) {
         this.aprovado = aprovado;
     }  
 }   
